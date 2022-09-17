7fb961e1237d8cec75d962396da277fce6fa1047
francescoP02 <file.partipilo@gmail.com>
2022-09-17 14:24:45 +0200

    add Quadrato

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int[] numeri = { 2, 6, 7, 5, 3, 9 };
+int[] numbers = { 2, 6, 7, 5, 3, 9 };
+int[] numbers2 = new int[numbers.Length];
 
-StampaArray(numeri);
-
+Console.WriteLine("Ecco la lista degli array");
+StampaArray(numbers);
 
 
 
@@ -20,11 +21,18 @@ void StampaArray(int[] array)
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(numeri[i]);
+        Console.Write(numbers[i]);
         if (i < array.Length - 1)
         {
             Console.Write(",");
         }
     }
     Console.Write("]");
+}
+
+
+int Quadrato(int numero)
+{
+
+    return numero * numero;
 }
