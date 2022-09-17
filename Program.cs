ce6be535f33e4d3a086b426cb9772f1a29d4c595
francescoP02 <file.partipilo@gmail.com>
2022-09-17 14:11:22 +0200

    Added StampaArray function

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+int[] numeri = { 2, 6, 7, 5, 3, 9 };
+
+StampaArray(numeri);
+
+
+
+
+
+
+
+
+//FUNZIONI
+
+//Funzione lettura Array
+
+void StampaArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(numeri[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]");
+}
