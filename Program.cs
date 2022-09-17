7ace7c8b9053524074fde7e6fd0b43d029af2074
francescoP02 <file.partipilo@gmail.com>
2022-09-17 14:38:28 +0200

    added ElevaArrayAlQuadrato

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 int[] numbers = { 2, 6, 7, 5, 3, 9 };
-int[] numbers2 = new int[numbers.Length];
 
-Console.WriteLine("Ecco la lista degli array");
+Console.WriteLine("Ecco la lista di numeri");
+
 StampaArray(numbers);
 
+int[] numbers2 = ElevaArrayAlQuadrato(numbers);
 
+Console.WriteLine();
 
+Console.WriteLine("Ecco la lista di numeri elevato al quadrato");
+
+StampaArray(numbers2);
+
+Console.WriteLine();
+
+Console.WriteLine("Ecco la lista di controllo dei numeri non elevati al quadrato");
+
+StampaArray(numbers);
 
 
 
@@ -21,7 +32,7 @@ void StampaArray(int[] array)
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(numbers[i]);
+        Console.Write(array[i]);
         if (i < array.Length - 1)
         {
             Console.Write(",");
@@ -30,9 +41,26 @@ void StampaArray(int[] array)
     Console.Write("]");
 }
 
+//Funzione elevazione a potenza
 
 int Quadrato(int numero)
 {
 
     return numero * numero;
+}
+
+//Funzione creazione array con numeri elevati al quadrato
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    int[] squared = new int[array.Length];
+
+    array.CopyTo(squared, 0);
+
+    for (int i = 0; i < squared.Length; i++)
+    {
+        squared[i] = Quadrato(array[i]);
+    }
+
+    return squared;
 }
